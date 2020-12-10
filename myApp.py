e6d5d597be4a628f595e39a08847e18a6268edc0
Lawrence Johny <lawrence.mk.johny@gmail.com>
2020-12-10 13:33:02 +0100

    fx app

--- myApp.py
+++ myApp.py
@@ -7,4 +7,4 @@ st.write("Get sentiment of FX pairs")
 user_input = st.text_input("What currency pair are you looking at?", "EURUSD")
 
 sentimentScore = st.slider("select a number") 
-st.write("You chose", sentimentScore)
+st.write("Sentiment for " + user_input, sentimentScore)
