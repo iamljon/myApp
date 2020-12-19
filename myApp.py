ff42ba20a423e7ab0fcd4f20da6da125002db944
Lawrence Johny <lawrence.mk.johny@gmail.com>
2020-12-19 22:16:59 +0100

    adding in vader sentiment

--- myApp.py
+++ myApp.py
@@ -1,10 +1,26 @@
 import streamlit as st
 
+st.sidebar.title("FX Sentiment Analysis")
+fx_pair = st.sidebar.text_input("Currency pair: ", "EURUSD")
 
-st.write("Get sentiment of FX pairs")
+fx_news = st.sidebar.text_area("Insert news for sentiment analysis", 'News in here')
 
 
-user_input = st.text_input("What currency pair are you looking at?", "EURUSD")
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyser = SentimentIntensityAnalyzer()
 
-sentimentScore = st.slider("select a number") 
-st.write("Sentiment for " + user_input, sentimentScore)
+fx_news_score = analyser.polarity_scores(fx_news)
+
+st.sidebar.write("Sentiment for " + fx_pair + ": ", fx_news_score["compound"])
+
+
+
+
+
+
+
+# st.write("Get sentiment of FX pairs")
+# user_input = st.text_input("What currency pair are you looking at?", "EURUSD")
+# sentimentScore = st.slider("select a number") 
+# st.write("Sentiment for " + user_input + ":  ", sentimentScore)
+# st.sidebar.title("here is the heading")
